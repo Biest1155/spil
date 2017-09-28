b7b2b6536e5ee18258b21f48e82d8744b63913ba
Mikkel Vejen Madsen <mikk934a@edu.eal.dk>
2017-09-28 13:12:40 +0200

    FINALBOSSTICTACTOE

--- spil/TicTacToe.cs
+++ spil/TicTacToe.cs
@@ -12,7 +12,7 @@ namespace spil
         public TicTacToe()
         {
             GameBoard = new char[3, 3] { {' ', ' ', ' '},
-                {' ', ' ', ' '}, 
+                {' ', ' ', ' '},
                 { ' ', ' ', ' '} };
         }
 
@@ -40,14 +40,77 @@ namespace spil
 
         public char Validate()
         {
+
             char resultat = ' ';
 
-            // skriv validerings kode her
+            // for-loop [0,i] i<3; i++  (Provided af Jepser)
+            // plads 1 == plads 2 && plads 2 == plads 3 && plads 3 != " "
 
+            for (int i = 0; i < 3; i++)
+            {
+                if (GameBoard[i, 2] == GameBoard[i, 2] && GameBoard[i, 2] == GameBoard[i, 2] && GameBoard[i, 2] != ' ')
+                {
+                    char tegn = GameBoard[i, 2];
+                    Console.WriteLine("WINNER is {0}",tegn);
+                }
+
+                if(GameBoard[0, i] == GameBoard[1, i] && GameBoard[1, i] == GameBoard[2, i] && GameBoard[2, i] != ' ')
+                {
+                    char tegn = GameBoard[1, i];
+                    Console.WriteLine("WINNER is {0}",tegn);
+                }
+
+                if ((GameBoard[0, 2] == 'x') && (GameBoard[1, 1] == 'x') && (GameBoard[2, 0] == 'x'))
+                {
+                    Console.WriteLine("WINNER is X");
+                }
+
+                if ((GameBoard[0, 2] == 'o') && (GameBoard[1, 1] == 'o') && (GameBoard[2, 0] == 'o'))
+                {
+                    Console.WriteLine("WINNER is O");
+                }
+
+                if ((GameBoard[2, 2] == 'x') && (GameBoard[1, 1] == 'x') && (GameBoard[0, 0] == 'x'))
+                {
+                    Console.WriteLine("WINNER is X");
+                }
+
+                if ((GameBoard[2, 2] == 'o') && (GameBoard[1, 1] == 'o') && (GameBoard[0, 0] == 'o'))
+                {
+                    Console.WriteLine("WINNER is O");
+                }
+            }
+
+           
             return resultat;
         }
 
-        // her kan implementeres metoder til at sætte og flytte en brik
+        
+        public void SætEnBrik(int v, int p, int player)
+        {
+            char tegn = ' ';
+
+            if (player == 1)
+            {
+                tegn = 'x';
+            }
+            else if (player == 2)
+            {
+                tegn = 'o';
+            }
+                       
+             
+            GameBoard[v, p] = tegn;
+                      
+
+            // her kan implementeres metoder til at sætte og flytte en brik
+
+
+        }
+
+        
+
+
 
     }
 }
--- spil/TicTacToeMenu.cs
+++ spil/TicTacToeMenu.cs
@@ -12,14 +12,14 @@ namespace spil
             do
             {
                 ShowMenu();
-                choice = GetUserChoise();
+                choice = GetUserChoice();
                 switch (choice)
                 {
                     case "1": DoActionFor1(); break;
                     case "2": DoActionFor2(); break;
                     case "3": DoActionFor3(); break;
                     case "0": running = false; break;
-                    default : ShowMenuSelectionErroe(); break;
+                    default : ShowMenuSelectionError(); break;
                 }
             } while (running);
         }
@@ -29,42 +29,69 @@ namespace spil
             Console.Clear();
             if (ticTacToe != null)
             {
+                
                 Console.WriteLine(ticTacToe.GetGameBoardView());
             }
-            Console.WriteLine("tic tac toe");
+            
+            Console.WriteLine("Tic Tac Toe");
             Console.WriteLine();
-            Console.WriteLine("1. Oret nyt spil");
-            Console.WriteLine("2. Set en brik");
+            Console.WriteLine("1. Opret nyt spil");
+            Console.WriteLine("2. Sæt en brik");
             Console.WriteLine("3. Flyt en brik");
             Console.WriteLine();
-            Console.WriteLine("0. exit");
+            Console.WriteLine("0. Exit");
+            
         }
 
-        private string GetUserChoise()
+        private string GetUserChoice()
         {
             Console.WriteLine();
             Console.Write("Indtast dit valg: ");
             return Console.ReadLine();
         }
 
-        private void ShowMenuSelectionErroe()
+        private void ShowMenuSelectionError()
         {
             Console.WriteLine("Ugyldigt valg.");
             Console.ReadLine();
         }
 
-        private void DoActionFor1()
+        private void DoActionFor1() //Opret nyt spil og slet det gamle.
         {
-            Console.WriteLine("skriv koden til at opret nyt spil");
-            Console.ReadLine();
+            ticTacToe = new TicTacToe();
+            
+
+            
         }
         private void DoActionFor2()
         {
-            Console.WriteLine("skriv koden til at få spillerens input til at sætte en brik");
-            Console.ReadLine();
+           
+            Console.WriteLine("Indtast hvilken spiller der skal tage sit træk");
+            string players = Console.ReadLine();
+            int player = Convert.ToInt32(players);
+
+            Console.WriteLine("Indstast nummer på x-akslen");
+
+            string vr = Console.ReadLine();
+            int v = Convert.ToInt32(vr);
+            v = v - 1;
+
+
+            Console.WriteLine("Indstast nummer på y-akslen");
+
+            string pr = Console.ReadLine();
+            int p = Convert.ToInt32(pr);
+            p = p - 1;
+            ticTacToe.SætEnBrik(v, p, player);
+
+            
+
+            //Console.WriteLine(ticTacToe.Validate());
+                    
         }
         private void DoActionFor3()
         {
+            
             Console.WriteLine("skriv koden til at få spillerens input til at flytte en brik");
             Console.ReadLine();
         }
